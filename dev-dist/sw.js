26ef14f22f77b027acf71c371328512993593f34
julljoll <julljoll@users.noreply.github.com>
2026-06-12 16:48:53 -0500

    utlmasss

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "./index.html",
-    "revision": "0.7h36uds6cgs"
+    "revision": "0.2bqrv0afr8g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("./index.html"), {
